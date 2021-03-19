3beb6b3299abf87f0bbdc5d8222aac4a0139df99
Breno Pereira <progbrcaua@gmail.com>
2021-03-19 00:03:00 -0300

    challenge finalized

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,32 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "../components/MovieCard";
+import MovieProps from "../App";
+
+interface ContentProps {
+  movies: MovieProps[];
+  genreTitle: string;
+}
+export function Content({ movies, genreTitle }: ContentProps) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {genreTitle}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
